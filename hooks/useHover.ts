1a2b93b30a5810f3a7c30d325821c4aec041f62b
huuyii <3134846106@qq.com>
2026-01-31 17:16:57 +0800

    feat: useHoverNew

--- hooks/useHover.ts
+++ hooks/useHover.ts
@@ -28,4 +28,32 @@ const useHover = (element: Element): [React.ReactElement, boolean] => {
   return [el, state];
 };
 
+export interface Options {
+  onEnter?: () => void;
+  onLeave?: () => void;
+  onChange?: (isHovering: boolean) => void;
+}
+
+const useHoverByRef = (
+  ref: React.RefObject<HTMLElement>,
+  options?: Options,
+): boolean => {
+  const { onEnter, onLeave, onChange } = options || {};
+  const [isEnter, setIsEnter] = React.useState(false);
+  React.useEffect(() => {
+    ref.current.addEventListener("mouseenter", () => {
+      onEnter?.();
+      setIsEnter(true);
+      onChange?.(true);
+    });
+    ref.current.addEventListener("mouseleave", () => {
+      onLeave?.();
+      setIsEnter(false);
+      onChange?.(false);
+    });
+  }, [ref]);
+
+  return isEnter;
+};
+
 export default useHover;
