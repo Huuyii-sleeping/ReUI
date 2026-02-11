e586ffd62b5023ccaadac9b709f6302cb3502852
huuyii <3134846106@qq.com>
2026-02-11 16:56:59 +0800

    feat: zustand

---
+++ utils/zustand.ts
@@ -0,0 +1,53 @@
+import * as React from "react";
+
+const createStore = (createState) => {
+  let state;
+  const listeners = new Set();
+
+  const setState = (partial, replace) => {
+    const nextState = typeof partial === "function" ? partial(state) : partial;
+
+    if (!Object.is(nextState, state)) {
+      const previousState = state;
+      if (replace) {
+        state =
+          typeof nextState !== "object" || nextState === null
+            ? nextState
+            : Object.assign({}, state, nextState);
+      } else {
+        state = nextState;
+      }
+      listeners.forEach((listener: any) => listener(state, previousState));
+    }
+  };
+
+  const getState = () => state;
+
+  const subscribe = (listener) => {
+    listeners.add(listener);
+    return () => listeners.delete(listener);
+  };
+
+  const destory = () => {
+    listeners.clear();
+  };
+
+  const api = { setState, getState, subscribe, destory };
+  state = createState(setState, getState, api);
+  return api;
+};
+
+function useStore(api, selector) {
+  function getState() {
+    return selector(api.getState());
+  }
+
+  return React.useSyncExternalStore(api.subscribe, getState);
+}
+
+export const create = (createState) => {
+  const api = createStore(createState);
+  const useBoundStore = (selector) => useStore(api, selector);
+  Object.assign(useBoundStore, api);
+  return useBoundStore;
+};
